914251e7a5979e2504cdcb1c50512a965b890258
arunkumar_rl <arunkumar_rl@github.com>
2020-02-13 21:42:10 +0100

    Todo App using Vue.js
    A todo-list app, created using vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,119 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>TO DO LIST</h1>
+    <md-field>
+      <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a Todo"></md-input>
+    </md-field>
+    <div class="todo-list">
+      <ul class="todos">
+        <li class="todo-item" v-for="todo in todos" :key="todo.id">
+          <span>
+            <input
+              class="completed"
+              type="checkbox"
+              @change="togglecompleted"
+              v-model="todo.completed"
+            />
+
+            <span
+              v-if="todo.editing === false"
+              class="todo-item-label"
+              :class="{'is-complete': todo.completed}"
+              @dblclick="editTodo(todo)"
+            >{{ todo.label }}</span>
+            <div v-if="todo.editing">
+              <md-field>
+                <md-input
+                  type="text"
+                  v-model="todo.label"
+                  @keyup.enter="stopEdit(todo)"
+                  @keyup.esc="stopEdit(todo)"
+                  @focusout="stopEdit(todo)"
+                  placeholder="EDIT TODO"
+                ></md-input>
+              </md-field>
+            </div>
+          </span>
+          <md-button class="md-raised md-ripple:false" @click="editTodo(todo)">Edit</md-button>
+          <md-button class="md-raised md-accent" @click="removeTodo(todo)">Delete</md-button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+      editedTodo: ""
+    };
+  },
+
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        editing: false
+      });
+      this.currentTodo = "";
+    },
+    togglecompleted() {
+      this.todos.completed = !this.todos.completed;
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      todo.editing = true;
+    },
+    stopEdit(todo) {
+      todo.editing = false;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 5% 10% !important;
 }
-</style>
+.container {
+  min-height: 100px;
+  background-color: gray;
+  padding: 2%;
+}
+.container h1 {
+  text-align: center;
+  margin-bottom: 50px;
+  text-decoration: underline;
+}
+.is-complete {
+  text-decoration: line-through;
+  color: black;
+}
+ul {
+  list-style-type: none;
+}
+.md-field .md-input,
+.md-field .md-textarea {
+  background-color: whitesmoke !important;
+}
+
+.todos .md-raised {
+  margin-top: -5px;
+}
+.md-button .md-ripple {
+  padding: -1px -1px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: gainsboro;
+}
+</style> 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdField, MdButton, MdCheckbox } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(MdField);
+Vue.use(MdButton);
+Vue.use(MdCheckbox);
+
+
 
 Vue.config.productionTip = false
 
